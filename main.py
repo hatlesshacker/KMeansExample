fe4c813226ca0db6afe76632f8ce40fcfe9716d1
warmachine028 <warmachine028@outlook.com>
2021-07-10 15:08:46 +0530

    Added new Functionalites here

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 """
 CHANGE LOG:
-- 8th July 2021
-- Tested with all files under data/
-- Fixed prediction functionality in clusters.py
-    -> HOW ?: Resolved some conflict of global and
-            local vars in update_epicenter
-    -> Removed s_down function and made minor changes
-- In auxiliary.py:
-    -> Renamed class var Points.cl to Point.cluster
-    -> Removed s_down function
-    -> Added __repr__() magic functions in 2 Classes
-- In points.py:
-    -> distance takes cluster and point directly to return distances
-    -> get_next directly returns Point instead of index of point
-    -> View now displays cluster contents before asking for user input
-- In predict.py:
-    -> Replaced use of s_down to f-string in predict function
-- In preview.py:
-    -> Fixed preview() function
-    -> Removed get_colours() and added itertools.cycle() iterator
-    -> Added few more colours to cycle through
+- 10th July 2021
+- Minor changes in code
+- Added:
+    -> tkinter GUI prompt for choosing csv files
+    -> Visualisation of Cluster epicenters
+
+- In main.py:
+    -> Increased assignment from 5 times to 7 times for better results
+    -> Removed 'csv' parameter from train()
+        as it's already globally available
+    -> Changed point list to tuple d-type for faster execution and reduce memory
+- In auxiliary.py
+    -> Changed Cluster points from mutable list to immutable tuple type
+       for better execution time and memory
+- In clusters.py
+    -> Replaced usage of get_points_cl to filter functions
+- In points.py
+    -> Replaced usage of math module with exponential operators
+    -> Removed get_points_cl function as it is deprecated now
 """
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename as aofn
 
 import modules.clusters as ct
 import modules.identify as identify
@@ -41,44 +43,48 @@ clusters: list[Cluster] = []
 # STEP 4: Assign point to cluster
 # STEP 5: Update epicenters
 # STEP 6: Repeat STEP 3 until epicenters don't change
-# STEP 7: Finalize Clusters
+# STEP 7: Finally assign all points to clusters
 
 # To initialize and set Points to Clusters
-def train(csv: str) -> None:
-    points: list[Point] = pt.setup(pt.get_points(csv))  # STEP 1: Obtain points
-    no_clusters: int = int(input("\nEnter the number of clusters clearly visible in plot: "))
+def train() -> None:
+    points: tuple[Point, ...] = pt.setup(pt.get_points(csv_file))  # STEP 1: Obtain points
+    n_clusters: int = int(input("\nEnter the number of clusters clearly visible in plot: "))
 
     clusters.clear()
-    clusters.extend([Cluster(n + 1) for n in range(no_clusters)])  # STEP 2: Create clusters
-    for _ in range(len(points) * 5):  # Run assignment
+    clusters.extend([Cluster(n + 1) for n in range(n_clusters)])  # STEP 2: Create clusters
+    for _ in range(len(points) * 7):  # Run assignment
         pt.assign_next(clusters, points)
         ct.update_epicenter(clusters, points)
 
     while True:  # STEP 3: Get next point to work on
         # record sum of previous epicenters:
-        pre_x = sum([cluster.epi_x for cluster in clusters])
-        pre_y = sum([cluster.epi_y for cluster in clusters])
+        pre_x = sum(cluster.epi_x for cluster in clusters)
+        pre_y = sum(cluster.epi_y for cluster in clusters)
 
         pt.assign_next(clusters, points)  # STEP 4: Assign point to cluster
         ct.update_epicenter(clusters, points)  # STEP 5: Update epicenters
 
         # record sum of new epicenters:
-        new_x = sum([cluster.epi_x for cluster in clusters])
-        new_y = sum([cluster.epi_y for cluster in clusters])
+        new_x = sum(cluster.epi_x for cluster in clusters)
+        new_y = sum(cluster.epi_y for cluster in clusters)
 
         # STEP 6: Repeat STEP 3 until epicenters don't change
         delta_xy = abs(pre_x - new_x) + abs(pre_y - new_y)
         if delta_xy == 0:
             break
 
-    ct.finalize(clusters, points)  # STEP 7: Finalize Clusters
+    # STEP 7: Finally assigning all points to clusters
+    ct.finalize(clusters, points)
     print("Hopefully, now the epicenters are correctly arranged")
     trained.set(True)
 
 
 if __name__ == '__main__':
+    Tk().withdraw()
     print(" ** \n Welcome to KMeansExample.\n **\n")
-    csv_file = input("Please enter the csv file containing the student records: ")
+    file_path = aofn(initialdir='data', title="Select a csv file", filetypes=(("csv files", "*.csv"),))
+    csv_file = file_path[file_path.rfind('/') - 4:]
+
     print(f"Working on student records at {csv_file}..")
     while True:
         print("\n  * (1) for Previewing the records")
@@ -97,7 +103,7 @@ if __name__ == '__main__':
             preview.preview(csv_file)
 
         elif choice == 2:  # Training data
-            train(csv_file)
+            train()
 
         elif choice == 3:  # Exiting Program
             break
